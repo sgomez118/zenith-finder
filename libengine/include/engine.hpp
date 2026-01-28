8088e91237704a97c3a0367b6aae22d69295ecc3
Scotty <scottygomez007@gmail.com>
2026-01-27 22:13:48 -0800

    Add zenith finder engine

---
+++ libengine/include/engine.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <string_view>
+#include <vector>
+#include <string>
+#include <array>
+
+namespace engine {
+
+struct Star {
+    std::string_view name;
+    double ra;  // Right Ascension in degrees
+    double dec; // Declination in degrees
+};
+
+struct CelestialResult {
+    std::string name;
+    double elevation;
+    double azimuth;
+    double zenith_dist;
+    bool is_rising;
+};
+
+struct Observer {
+    double latitude;
+    double longitude;
+    double altitude;
+};
+
+// The 50 brightest stars catalog
+constexpr std::array<Star, 50> StarCatalog = {{
+    {"Sirius", 101.287, -16.716},
+    {"Canopus", 95.988, -52.696},
+    {"Rigil Kentaurus", 219.902, -60.833},
+    {"Arcturus", 213.915, 19.182},
+    {"Vega", 279.235, 38.784},
+    {"Capella", 79.172, 45.998},
+    {"Rigel", 78.634, -8.202},
+    {"Procyon", 114.825, 5.225},
+    {"Achernar", 24.429, -57.237},
+    {"Betelgeuse", 88.793, 7.407},
+    {"Hadar", 210.951, -60.373},
+    {"Altair", 297.696, 8.868},
+    {"Acrux", 186.649, -63.099},
+    {"Aldebaran", 68.98, 16.509},
+    {"Antares", 247.352, -26.432},
+    {"Spica", 201.298, -11.161},
+    {"Pollux", 116.329, 28.026},
+    {"Fomalhaut", 344.413, -29.622},
+    {"Deneb", 310.358, 45.28},
+    {"Mimosa", 191.93, -59.689},
+    {"Regulus", 152.093, 11.967},
+    {"Adhara", 104.656, -28.972},
+    {"Castor", 113.65, 31.888},
+    {"Gacrux", 187.791, -57.113},
+    {"Shaula", 263.402, -37.103},
+    {"Bellatrix", 81.283, 6.349},
+    {"Elnath", 81.573, 28.608},
+    {"Miaplacidus", 138.3, -69.717},
+    {"Alnilam", 84.053, -1.202},
+    {"Alnair", 332.058, -46.961},
+    {"Alnitak", 85.19, -1.943},
+    {"Alioth", 193.507, 55.959},
+    {"Dubhe", 165.93, 61.751},
+    {"Mirfak", 51.081, 49.857},
+    {"Wezen", 106.381, -26.393},
+    {"Kaus Australis", 276.191, -34.385},
+    {"Avior", 125.628, -59.513},
+    {"Alkaid", 206.885, 49.313},
+    {"Sargas", 264.33, -42.998},
+    {"Menkalinan", 89.867, 44.947},
+    {"Atria", 252.166, -69.028},
+    {"Alhena", 99.428, 16.399},
+    {"Peacock", 306.412, -56.735},
+    {"Alsephina", 137.047, -47.343},
+    {"Mirzam", 95.675, -17.956},
+    {"Alphard", 141.897, -8.658},
+    {"Polaris", 37.946, 89.264},
+    {"Hamal", 31.681, 23.462},
+    {"Algieba", 154.993, 19.842},
+    {"Diphda", 10.897, -17.986}
+}};
+
+#include <chrono>
+
+class AstrometryEngine {
+public:
+    static std::vector<CelestialResult> calculate_zenith_proximity(const Observer& obs, std::chrono::system_clock::time_point time = std::chrono::system_clock::now());
+};
+
+} // namespace engine
---
+++ libengine/src/engine.cpp
@@ -0,0 +1,79 @@
+#include <chrono>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include "engine.hpp"
+
+extern "C" {
+#include <novas.h>
+}
+
+namespace engine {
+
+// Helper to convert C++20 chrono time to Julian Date using NOVAS
+double get_julian_date(std::chrono::system_clock::time_point tp) {
+    auto sd = std::chrono::floor<std::chrono::days>(tp);
+    auto time = tp - sd;
+    
+    std::chrono::year_month_day ymd{sd};
+    int year = static_cast<int>(ymd.year());
+    unsigned month = static_cast<unsigned>(ymd.month());
+    unsigned day = static_cast<unsigned>(ymd.day());
+    
+    double hour = std::chrono::duration<double, std::ratio<3600>>(time).count();
+    
+    return julian_date((short)year, (short)month, (short)day, hour);
+}
+
+std::vector<CelestialResult> AstrometryEngine::calculate_zenith_proximity(const Observer& obs, std::chrono::system_clock::time_point time) {
+    // Use built-in low-precision ephemeris for Earth/Sun
+    set_planet_provider(earth_sun_calc);
+    set_planet_provider_hp(earth_sun_calc_hp);
+    
+    std::vector<CelestialResult> results;
+    
+    double jd_utc = get_julian_date(time);
+    
+    // For simplicity in v0.1, we'll use delta_t = 69.184 (approximate for 2024-2026)
+    double delta_t = 69.184;
+    double jd_tt = jd_utc + delta_t / 86400.0;
+    
+    on_surface site;
+    make_on_surface(obs.latitude, obs.longitude, obs.altitude, 10.0, 1010.0, &site);
+    
+    observer novas_obs;
+    make_observer_at_site(&site, &novas_obs);
+
+    for (const auto& star : StarCatalog) {
+        cat_entry star_cat;
+        make_cat_entry(star.name.data(), "J2000", 0, star.ra / 15.0, star.dec, 0, 0, 0, 0, &star_cat);
+
+        sky_pos topo_pos;
+        int error = place_star(jd_tt, &star_cat, &novas_obs, delta_t, NOVAS_TOD, NOVAS_REDUCED_ACCURACY, &topo_pos);
+
+        if (error == 0) {
+            double zd, az, rar, decr;
+            equ2hor(jd_utc, delta_t, NOVAS_REDUCED_ACCURACY, 0.0, 0.0, &site, topo_pos.ra, topo_pos.dec, NOVAS_WEATHER_AT_LOCATION, &zd, &az, &rar, &decr);
+
+            double el = 90.0 - zd;
+            if (el > 0) { // Only objects above the horizon
+                CelestialResult res;
+                res.name = std::string(star.name);
+                res.azimuth = az;
+                res.elevation = el;
+                res.zenith_dist = zd;
+                res.is_rising = (az < 180.0);
+                results.push_back(res);
+            }
+        }
+    }
+
+    // Sort by zenith distance (ascending)
+    std::sort(results.begin(), results.end(), [](const CelestialResult& a, const CelestialResult& b) {
+        return a.zenith_dist < b.zenith_dist;
+    });
+
+    return results;
+}
+
+} // namespace engine
